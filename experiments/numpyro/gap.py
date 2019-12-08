2a75ece5fdde82041a68e3472164ba45edb6a677
Dmitry Vukolov <dmvuko@gmail.com>
2019-12-08 04:09:03 +0300

    Added an example with missing data

---
+++ experiments/numpyro/gap.py
@@ -0,0 +1,100 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# Install NumPyro by running:
+# `$ pip install --upgrade numpyro`
+
+# +
+from functools import partial
+
+# Import regular numpy in additional to JAX's
+import numpy
+import scipy.stats
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# %run helpers.ipynb
+
+# +
+# Compute on a CPU using 2 cores
+numpyro.set_platform('cpu')
+numpyro.set_host_device_count(2)
+
+# Make plots larger by default
+plt.rc('figure', dpi=100)
+
+# +
+# Define the true function and generate observations
+func = lambda x: scipy.stats.norm(loc=0.1 * x**3, scale=0.5)
+func.latex = r'$y_i = 0.1x_i^3 + \varepsilon$'
+
+data_points = [
+    { 'n_points': 40, 'xlim': [-4, -1] },
+    { 'n_points': 40, 'xlim': [1, 4] },
+]
+df = generate_data(func, points=data_points, seed=4)
+
+# Plot the data
+plot_true_function(func, df, title=f'True Function: {func.latex}')
+
+# +
+# Observations
+X = df[['x']].values
+Y = df[['y']].values
+X_test = numpy.linspace(X.min(), X.max(), num=1000)[:, np.newaxis]
+
+# Number of hidden layers
+hidden = 1
+# Width of hidden layers
+width = 10
+# Standard deviation of the prior
+sigma = 2.0
+# Standard deviation of the likelihood
+noise = 0.5
+
+# Instantiate the model with prior standard deviation and likelihood noise
+model = partial(feedforward, X=X, Y=Y, width=width, hidden=hidden, sigma=sigma, noise=noise)
+# -
+
+# # No-U-Turn Sampling
+
+# +
+# %%time
+# NUTS parameters
+num_chains = 2
+num_samples = 2000
+num_warmup = 2000
+
+# Run the No-U-Turn sampler. Note: sampling more than one chain in parallel doesn't show a progress bar
+mcmc = sample(model, num_samples, num_warmup, num_chains, seed=0, summary=True)
+# -
+
+# Generate the posterior predictive and plot the results
+posterior_predictive = simulate_posterior_predictive(model, mcmc, X_test, seed=1)
+plot_posterior_predictive(X_test, posterior_predictive, func=func, df=df,
+                          title=f'NUTS, Weight Uncertainty {sigma}, Noise {noise},\n'
+                                f'{width} Nodes in {hidden} Hidden Layer')
+# # Variational Inference
+
+# Approximate the posterior using Automatic Differentiation Variational Inference
+vi = advi(model, num_iter=500_000, learning_rate=0.001, seed=0)
+vi.plot_loss()
+
+# Generate the posterior predictive and plot the results
+posterior_predictive = simulate_posterior_predictive(model, vi, X_test, n_samples=1000, seed=1)
+plot_posterior_predictive(X_test, posterior_predictive, func=func, df=df,
+                          title=f'ADVI, Weight Uncertainty {sigma}, Noise {noise},\n'
+                                f'{width} Nodes in {hidden} Hidden Layer')
